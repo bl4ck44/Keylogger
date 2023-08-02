17dbeb3c6a84a0d359f35199df706ac16f98d03b
bl4ck44 <hackingbl4ck44@gmail.com>
2023-08-01 21:04:47 -0600

    Version 1

--- setup.sh
+++ setup.sh
@@ -2,8 +2,4 @@ clear && figlet Keylogger | lolcat
 
 echo
 
-git clone https://github.com/brentvollebregt/auto-py-to-exe.git
-cd auto-py-to-exe
-sudo python setup.py install
-cd ..
-sudo rm -r auto-py-to-exe
+pip3 install pyinstaller
